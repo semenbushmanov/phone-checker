4b9b1385062a4064fc014e42ca846d3e625b8411
Semen Bushmanov <bush.sem@mail.ru>
2025-04-06 19:27:26 +0500

    Add phone normalization and validation to php phone checker

--- index.php
+++ index.php
@@ -3,25 +3,33 @@ $blocked_phones = ['+71111111111', '+72222222222', '+73333333333', '+74444444444
 $phone_to_check = readline("Input a phone number to check if it's blocked: ");
 $blocked = false;
 
-$left_edge = 0;
-$right_edge = count($blocked_phones) - 1;
+$phone_to_check = preg_replace('/\D+/', '', $phone_to_check);
 
-while (($left_edge <= $right_edge) && ($blocked == false)) {
-    $middle_point = intdiv($left_edge + $right_edge, 2);
-    $blocked_phone = $blocked_phones[$middle_point];
+if (preg_match('/^8(\d{10})$/', $phone_to_check, $matched_phone) || preg_match('/^7(\d{10})$/', $phone_to_check, $matched_phone)) {
+    $phone_to_check = '+7' . $matched_phone[1];
 
-    if ($phone_to_check == $blocked_phone) {
-        $blocked = true;
-    } elseif ($phone_to_check > $blocked_phone) {
-        $left_edge = $middle_point + 1;
-    } else {
-        $right_edge = $middle_point - 1;
+    $left_edge = 0;
+    $right_edge = count($blocked_phones) - 1;
+
+    while (($left_edge <= $right_edge) && ($blocked == false)) {
+        $middle_point = intdiv($left_edge + $right_edge, 2);
+        $blocked_phone = $blocked_phones[$middle_point];
+
+        if ($phone_to_check == $blocked_phone) {
+            $blocked = true;
+        } elseif ($phone_to_check > $blocked_phone) {
+            $left_edge = $middle_point + 1;
+        } else {
+            $right_edge = $middle_point - 1;
+        }
     }
-}
 
-if ($blocked) {
-    echo 'block';
+    if ($blocked) {
+        echo 'block';
+    } else {
+        echo 'ok';
+    }
 } else {
-    echo 'ok';
+    echo 'Invalid phone number';
 }
 ?>
