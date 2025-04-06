21d877b9368ce5eaf264bb814c784419d5bde441
Semen Bushmanov <bush.sem@mail.ru>
2025-04-06 18:22:01 +0500

    Add binary search algorithm to php phone checker

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
-$blocked_phones = ['+71111111111', '+72222222222', '+73333333333'];
-$phone_to_check = readline("Введите номер телефона для проверки: ");
+$blocked_phones = ['+71111111111', '+72222222222', '+73333333333', '+74444444444', '+75555555555', '+76666666666', '+77777777777'];
+$phone_to_check = readline("Input a phone number to check if it's blocked: ");
 $blocked = false;
 
-foreach ($blocked_phones as $blocked_phone) {
+$left_edge = 0;
+$right_edge = count($blocked_phones) - 1;
+
+while (($left_edge <= $right_edge) && ($blocked == false)) {
+    $middle_point = intdiv($left_edge + $right_edge, 2);
+    $blocked_phone = $blocked_phones[$middle_point];
+
     if ($phone_to_check == $blocked_phone) {
         $blocked = true;
+    } elseif ($phone_to_check > $blocked_phone) {
+        $left_edge = $middle_point + 1;
+    } else {
+        $right_edge = $middle_point - 1;
     }
 }
 
